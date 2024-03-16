c645991724671f49657c251445f073edf801f192
JCE98 <josiah@raecodesign.com>
2024-03-16 14:20:55 -0500

    DQN training framework

---
+++ p1_navigation/DeepRLND-NavigationTraining.py
@@ -0,0 +1,147 @@
+'''
+Udacity Deep Reinforcement Learning Nanodegree
+Navigation Project
+Author: Josiah Everhart
+Objective: Train a deep reinforcement learning agent to navigate a world, collecting yellow bananas and avoiding blue bananas, using value based optimization methods
+'''
+
+#Import Libraries
+from unityagents import UnityEnvironment
+import numpy as np
+import sys, os, torch, time
+from collections import deque
+import matplotlib.pyplot as plt
+from dqn_agent import Agent
+
+def dqn(env, agent, brain_name, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    '''
+    Deep Q-Learning
+    
+    Params
+    ======
+        env (user-defined Unity-ML environment class): Unity-ML environment in which the agent is to be trained
+        agent (user-defined Agent class): defines agent's interaction with the environment
+        brain_name (str): name of the brain deciding the agent's actions in the environment
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    '''
+    scores = []                                            # list containing scores from each episode
+    scores_window = deque(maxlen=100)                      # last 100 scores
+    eps = eps_start                                        # initialize epsilon
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=True)[brain_name]  # reset the environment in training mode
+        state = env_info.vector_observations[0]            # get the current state
+        score = 0                                          # initialize score
+        for t in range(max_t):
+            action = agent.act(state, eps)                 # agent takes action
+            action = action.astype(np.int32)
+            env_info = env.step(action)[brain_name]        # send the action to the environment
+            #TODO: Log states, actions, and rewards for policy development
+            next_state = env_info.vector_observations[0]   # get the next state
+            reward = env_info.rewards[0]                   # get the reward
+            done = env_info.local_done[0]                  # see if episode has finished
+            score += reward                                # update the score
+            state = next_state                             # roll over the state to next time step
+            if done:                                       # exit loop if episode finished
+                break
+        scores_window.append(score)                        # save most recent score
+        scores.append(score)                               # save most recent score
+        eps = max(eps_end, eps_decay*eps)                  # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.\
+              format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:                           #Taking an average score every 100 episodes
+            print('\rEpisode {}\tAverage Score: {:.2f}'.\
+                  format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>=200.0:                  #Checking if environment has been solved
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+            break
+    return scores
+
+def DemoTrainedAgent(env, agent, brain_name, agentFile, demoEpisodes=3, max_t=200):
+    '''
+    Demonstrate Trained Agent Performance
+    
+    Params
+    ======
+        env (user-defined Unity-ML environment class): Unity-ML environment in which the agent is to be trained
+        agent (user-defined Agent class): defines agent's interaction with the environment
+        brain_name (str): name of the brain deciding the agent's actions in the environment
+        agentFile (str): path to the trained agent checkpoint .pth file that contains the trained weights
+        demoEpisodes (int): maximum number of demonstration episodes
+        max_t (int): maximum number of time steps per episode
+    '''
+    agent.qnetwork_local.load_state_dict(torch.load(agentFile))
+    for i_episode in range(demoEpisodes):
+        env_info = env.reset(train_mode=False)[brain_name] # reset the environment in demonstration mode
+        state = env_info.vector_observations[0]            # get the current state
+        score = 0                                          # initialize score
+        for t in range(max_t):
+            action = agent.act(state)                      # trained agent takes action
+            action = action.astype(np.int32)
+            env_info = env.step(action)[brain_name]        # send the action to the environment
+            next_state = env_info.vector_observations[0]   # get the next state
+            done = env_info.local_done[0]                  # see if episode has finished
+            state = next_state                             # roll over the state to next time step
+            if done:                                       # exit loop if episode finished
+                break
+
+
+if __name__ == "__main__":
+    #Initialize environment
+    projectFilesPath = os.path.dirname(sys.argv[0])
+    env = UnityEnvironment(file_name = projectFilesPath + "/Banana_Windows_x86_64/Banana.exe")
+
+    #Get the default brain
+    brain_name = env.brain_names[0]
+    brain = env.brains[brain_name]
+
+    # reset the environment
+    print("\n")
+    env_info = env.reset(train_mode=False)[brain_name]
+
+    # environment details
+    print('\nNumber of agents:', len(env_info.agents))
+    action_size = brain.vector_action_space_size
+    print('Number of actions:', action_size)
+    state = env_info.vector_observations[0]
+    print('States look like:', state)
+    state_size = len(state)
+    print('States have length:', state_size)
+    print("\n")
+
+    #Agent Creation
+    agent = Agent(state_size,action_size,seed=0)
+
+    #Training or Demonstration
+    while True:
+        response = input("Would you like to train a new agent from scratch, or demonstrate a trained agent? (train or demo): ")
+        if response == "train":
+            #Train Agent
+            scores = dqn(env,agent,brain_name=env.brain_names[0])
+
+            #Plotting Training Results
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            plt.plot(np.arange(len(scores)), scores)
+            plt.ylabel('Score')
+            plt.xlabel('Episode #')
+            plt.show()
+            plt.savefig(time.strftime("%Y%m%d-%H%M%S")+"-training.png")
+            break
+        elif response == "demo":
+            #Demonstrate Trained Agent
+            while True:
+                agentFile = input("Enter the path to the trained agent checkpoint .pth file: ")
+                if not os.path.exists(agentFile):
+                    input("The file path you entered could not be found. Please check the path and try again: ")
+                else:
+                    break
+            DemoTrainedAgent(env,agent,brain_name,agentFile)
+            break
+        else:
+            response = input("Please enter a valid response (demo or train): ")
+    env.close()
